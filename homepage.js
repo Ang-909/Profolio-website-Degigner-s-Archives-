b7aafff19a0a6d4eadd7933b531ee2daafd74a91
nt36744 <nt367@njit.edu>
2024-12-07 20:09:49 -0500

    Added Js to homepage

--- homepage.js
+++ homepage.js
@@ -1,4 +1,4 @@
 function updateTitle() {
-    const newtitle = document.getElementById('newtitle').value;
-    document.getElementById('title').innerText = newtitle;
-  }
+    const newtitle = document.getElementById('username').value; // Get the value entered in the username field
+    document.getElementById('Title').innerText = 'Welcome, ' + newtitle; // Set the inner text of the title element to the entered username
+}
